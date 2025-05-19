8f9c3415de4e29d4354ceb640f53c64bebe88cbd
Aquib Baig <aquibbaig97@gmail.com>
2025-05-19 23:54:36 +0530

    neogen and ts update

--- lua/aquib/plugins/ts.lua
+++ lua/aquib/plugins/ts.lua
@@ -1,2 +1,32 @@
-return { "nvim-treesitter/nvim-treesitter" }
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    require("nvim-treesitter.configs").setup {
+      ensure_installed = {
+        "lua",
+        "python",
+        "javascript",
+        "typescript",
+        "go",
+        "rust",
+        "c",
+        "cpp",
+        "java",
+        "bash",
+        "json",
+        "html",
+        "css",
+      },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true
+      }
+    }
+  end
+}
 
