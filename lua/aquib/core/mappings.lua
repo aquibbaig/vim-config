6cc8f015c3c3908634fa35027a0cf6f168d07acb
Aquib Baig <aquibbaig97@gmail.com>
2025-05-24 17:03:46 +0530

    neotree

--- lua/aquib/core/mappings.lua
+++ lua/aquib/core/mappings.lua
@@ -1,50 +1,51 @@
 -- ── Treewalker: Node Movement ────────────────────────────────────────────────
 -- Move to the syntactic node *above* the current one
-vim.keymap.set({ 'n', 'v' }, '<C-k>', '<cmd>Treewalker Up<cr>', {
-  silent = true,
-  desc = 'Treewalker: Move to parent (Up)'
+vim.keymap.set({ "n", "v" }, "<C-k>", "<cmd>Treewalker Up<cr>", {
+	silent = true,
+	desc = "Treewalker: Move to parent (Up)",
 })
 
 -- Move to the syntactic node *below* the current one
-vim.keymap.set({ 'n', 'v' }, '<C-j>', '<cmd>Treewalker Down<cr>', {
-  silent = true,
-  desc = 'Treewalker: Move to child (Down)'
+vim.keymap.set({ "n", "v" }, "<C-j>", "<cmd>Treewalker Down<cr>", {
+	silent = true,
+	desc = "Treewalker: Move to child (Down)",
 })
 
 -- Move to the *previous sibling* in the syntax tree
-vim.keymap.set({ 'n', 'v' }, '<C-h>', '<cmd>Treewalker Left<cr>', {
-  silent = true,
-  desc = 'Treewalker: Move to previous sibling (Left)'
+vim.keymap.set({ "n", "v" }, "<C-h>", "<cmd>Treewalker Left<cr>", {
+	silent = true,
+	desc = "Treewalker: Move to previous sibling (Left)",
 })
 
 -- Move to the *next sibling* in the syntax tree
-vim.keymap.set({ 'n', 'v' }, '<C-l>', '<cmd>Treewalker Right<cr>', {
-  silent = true,
-  desc = 'Treewalker: Move to next sibling (Right)'
+vim.keymap.set({ "n", "v" }, "<C-l>", "<cmd>Treewalker Right<cr>", {
+	silent = true,
+	desc = "Treewalker: Move to next sibling (Right)",
 })
 
 -- ── Treewalker: Node Swapping ────────────────────────────────────────────────
 -- Swap current node with the one *above* it in the tree
-vim.keymap.set('n', '<C-S-k>', '<cmd>Treewalker SwapUp<cr>', {
-  silent = true,
-  desc = 'Treewalker: Swap with parent (Up)'
+vim.keymap.set("n", "<C-S-k>", "<cmd>Treewalker SwapUp<cr>", {
+	silent = true,
+	desc = "Treewalker: Swap with parent (Up)",
 })
 
 -- Swap current node with the one *below* it (i.e., its child)
-vim.keymap.set('n', '<C-S-j>', '<cmd>Treewalker SwapDown<cr>', {
-  silent = true,
-  desc = 'Treewalker: Swap with child (Down)'
+vim.keymap.set("n", "<C-S-j>", "<cmd>Treewalker SwapDown<cr>", {
+	silent = true,
+	desc = "Treewalker: Swap with child (Down)",
 })
 
 -- Swap current node with the *previous sibling*
-vim.keymap.set('n', '<C-S-h>', '<cmd>Treewalker SwapLeft<cr>', {
-  silent = true,
-  desc = 'Treewalker: Swap with previous sibling (Left)'
+vim.keymap.set("n", "<C-S-h>", "<cmd>Treewalker SwapLeft<cr>", {
+	silent = true,
+	desc = "Treewalker: Swap with previous sibling (Left)",
 })
 
 -- Swap current node with the *next sibling*
-vim.keymap.set('n', '<C-S-l>', '<cmd>Treewalker SwapRight<cr>', {
-  silent = true,
-  desc = 'Treewalker: Swap with next sibling (Right)'
+vim.keymap.set("n", "<C-S-l>", "<cmd>Treewalker SwapRight<cr>", {
+	silent = true,
+	desc = "Treewalker: Swap with next sibling (Right)",
 })
 
+vim.keymap.set("n", "<C-b>", "<Cmd>Neotree toggle<CR>")
--- lua/aquib/plugins/mason.lua
+++ lua/aquib/plugins/mason.lua
@@ -1,37 +1,74 @@
 return {
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	lazy = false,
-	config = function()
-		local servers = {"lua_ls", "prettier"}
-
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = servers
-		})
-
-		local lsp = require("lspconfig")
-
-		local function on_attach(_, bufnr)
-			local opts = { buffer = bufnr, remap = false }
-			vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-			vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
-			vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-			vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-			vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-			vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-			vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-			vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-			vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-			vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-			vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-		end
-
-		for _, lsp_server in ipairs(servers) do
-			lsp[lsp_server].setup({
-				on_attach = on_attach
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		lazy = false,
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "tailwindcss" },
 			})
-		end
-	end
-}
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		lazy = false,
+		config = function()
+			local lspconfig = require("lspconfig")
+			local servers = { "lua_ls", "tailwindcss" }
 
+			local function on_attach(_, bufnr)
+				local opts = { buffer = bufnr, remap = false }
+				vim.keymap.set("n", "gd", function()
+					vim.lsp.buf.definition()
+				end, opts)
+				vim.keymap.set("n", "gi", function()
+					vim.lsp.buf.implementation()
+				end, opts)
+				vim.keymap.set("n", "K", function()
+					vim.lsp.buf.hover()
+				end, opts)
+				vim.keymap.set("n", "<leader>vws", function()
+					vim.lsp.buf.workspace_symbol()
+				end, opts)
+				vim.keymap.set("n", "<leader>vd", function()
+					vim.diagnostic.open_float()
+				end, opts)
+				vim.keymap.set("n", "[d", function()
+					vim.diagnostic.goto_next()
+				end, opts)
+				vim.keymap.set("n", "]d", function()
+					vim.diagnostic.goto_prev()
+				end, opts)
+				vim.keymap.set("n", "<leader>vca", function()
+					vim.lsp.buf.code_action()
+				end, opts)
+				vim.keymap.set("n", "<leader>vrr", function()
+					vim.lsp.buf.references()
+				end, opts)
+				vim.keymap.set("n", "<leader>vrn", function()
+					vim.lsp.buf.rename()
+				end, opts)
+				vim.keymap.set("n", "<C-h>", function()
+					vim.lsp.buf.signature_help()
+				end, opts)
+			end
+
+			for _, lsp_server in ipairs(servers) do
+				lspconfig[lsp_server].setup({
+					on_attach = on_attach,
+				})
+			end
+		end,
+	},
+}
